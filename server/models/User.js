639288cb984b79665b9bffe5078f655302002a2c
Agbajestephen <agbajestephen5@gmail.com>
2025-10-15 03:48:03 +0100

    update

--- server/models/User.js
+++ server/models/User.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const router = require('../routes');
+const mongoose = require("mongoose");
+const router = require("../routes");
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    googleId: {
-        type: String,
-        requires: true
-    },
-    displayName: {
-        type: String,
-        requires: true
-    },
-    FirstName: {
-        type: String,
-        requires: true
-    },
-    lastName: {
-        type: String,
-        requires: true
-    },
-    profileImage: {
-        type: String,
-        requires: true
-    },
-    createdAt: {
-        type: Date,
-        default:Date.now
-    }
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  FirstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
